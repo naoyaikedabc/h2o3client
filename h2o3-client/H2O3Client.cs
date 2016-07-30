3690df10024073fd87961c9ece629bd4608abadc
Naoya Ikeda <n_ikeda@hotmail.com>
2016-07-30 06:28:25 +0000

    Initial commit

---
+++ h2o3-client/H2O3Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace Brainchild.H2O
+{
+    public class H2O3Client
+    {
+        RestClient client;
+
+        public H2O3Client(string endPoint)
+        {
+            this.client = new RestClient(endPoint);
+        }
+
+        public H2O3Client(): this("http://localhost:54321/")
+        {
+
+        }
+
+    }
+}
